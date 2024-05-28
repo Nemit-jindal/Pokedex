a03cb8403e42ea7f75a64227c21dfa62b4724ae4
Nemit-jindal <nemitjindal5016@gmail.com>
2024-05-29 00:18:25 +0530

    first commit

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,12 @@
+import useDebounce from '../../hooks/useDebounce'
+import './Search.css'
+
+function Search({updateSearchTerm}){
+    const debounceUpdateSearch=useDebounce((e)=>updateSearchTerm(e.target.value))
+    return(
+        <input id='search-pokemon' type='text' placeholder='Pokemon' onChange={debounceUpdateSearch} />
+    )
+
+}
+
+export default Search
